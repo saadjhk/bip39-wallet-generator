2abab53a240a08e8d1870cb59da0683e07da8ebd
Saad Ahmed <saadahm.siddiqui@gmail.com>
2023-03-14 02:29:21 +0500

    Added CLI

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,31 @@
 import { generateMnemonic } from 'bip39'
+import { program } from 'commander';
 import { ethers } from 'ethers'
-import prompt from 'prompt'
 
-function onErr(err: any) {
-    console.log(err);
-    process.exit(1)
-}
+program
+    .name('bip39-wallet-utils')
+    .description('CLI to work with BIP39 wallets')
+    .version('0.0.0');
 
-function promptCallback(err: any, result: { generate_again?: string }) {
-    if (err) { return onErr(err); }
+program.command('generate_mnemonic')
+    .description('Generate a wallet mnemonic')
+    .option('-k, --key-strength <key_strength>', 'Strength of the phrase', '128')
+    .action((args) => {
+        const mnemonic = generateMnemonic(
+            Number(args.keyStrength)
+        )
+        const wallet = ethers.Wallet.fromPhrase(mnemonic)
 
-    if (result.generate_again?.toLowerCase().trim() === 'y') {
-        generateWallet()
-    } else if (result.generate_again?.toLowerCase().trim() === 'n') {
-        process.exit(0)
-    }
+        console.log('Mnemonic: ' + mnemonic)
+        console.log('EVM Address: ' + wallet.address)
+    });
 
-    prompt.get(['generate_again'], promptCallback);
-}
+program.command('generate_publickey')
+    .description('Generate a wallet mnemonic')
+    .requiredOption('-m, --mnemonic <phrase>', 'Mnemonic phrase')
+    .action((args) => {
+        const wallet = ethers.Wallet.fromPhrase(args.mnemonic)
+        console.log('EVM Address: ' + wallet.address)
+    });
 
-function generateWallet() {
-    const mnemonic = generateMnemonic(256)
-    const wallet = ethers.Wallet.fromPhrase(mnemonic)
-
-    console.log('Mnemonic: ' + mnemonic)
-    console.log('EVM Address: ' + wallet.address)
-}
-
-
-prompt.start();
-generateWallet();
-prompt.get(['generate_again'], promptCallback);
+program.parse();
